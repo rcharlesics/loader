4e3cda5268f66509574ada12bf199e743d3eaba2
Raheem <rcharles@ics.com>
2020-11-17 12:04:36 -0500

    Created a list view for each loader.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "arcdata.h"
+#include <QQmlContext>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
-
+    qmlRegisterType<ArcData>("ArcNet", 1, 0, "ArcData");
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
@@ -15,6 +17,9 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
+
+
+
 
     return app.exec();
 }
